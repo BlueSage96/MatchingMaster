3fbea124078592e1518631c2f82379b588d67aee
Brittany Price <bprice0496@gmail.com>
2025-06-10 22:31:28 -0400

    Tested color matching - working

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import './css/App.css'
+import './css/App.css';
+import Match from './features/Match';
 
 function App() {
   return (
     <>
-      
+      <h1>Memory Game</h1>
+      <div
+        style={{
+          width: '800px',
+          height: '600px',
+          backgroundColor: 'tan',
+        }}
+      >
+        <Match />
+      </div>
     </>
-  )
+  );
 }
 
 export default App;
--- src/shared/Card.jsx
+++ src/shared/Card.jsx
@@ -1,4 +1,19 @@
-function Card () {
+import '../css/Card.css';
 
+/*
+    Functionality for flipping cards
+*/ 
+function Card ({onClick, color, flipped}) {
+    return (
+        <>
+            <div className="card-container" onClick={onClick}>
+                {/* Card flip state */}
+                <div className={`card-inner ${flipped ? 'flipped' : ''}`}>
+                    <div className="card-front" style={{ backgroundColor: color }}></div>
+                    <div className="card-back"></div>
+                </div>
+            </div>
+        </>
+    )
 }
 export default Card;
