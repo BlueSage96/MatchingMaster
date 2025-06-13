1029da6185be37280a0d1b048362251bb57c2d07
Brittany Price <bprice0496@gmail.com>
2025-06-13 03:06:38 -0400

    Added button sounds & nav bar adjustments

--- src/features/NavBar.jsx
+++ src/features/NavBar.jsx
@@ -1,42 +1,39 @@
 import { NavLink } from 'react-router-dom';
 import NavStyle from '../css/modules/NavBar.module.css';
 import Gear from '../assets/settings.png';
-import Home from '../assets/home.png';
 import Info from '../assets/instructions.png';
+import Home from '../assets/home.png';
+import ButtonSound from '../shared/ButtonSound';
 
 function NavBar() {
   return (
-    <nav className={NavStyle.navBar}>
-      <NavLink
-        to="/settings"
-        className={({ isActive }) =>
-          isActive ? `${NavStyle.navItem} ${NavStyle.activeLink}` : NavStyle.navItem
-        }
-      >
-        <img className={NavStyle.Settings} src={Gear} alt="Settings" />
-        <span className={NavStyle.SetText}>Settings</span>
-      </NavLink>
+    <>
+      <div className={NavStyle.NavBar}>
+        <NavLink to="/settings"
+          className={({ isActive }) =>`${NavStyle.NavItem} ${isActive ? NavStyle.activeLink : ''}`}>
+          <ButtonSound invisible className={NavStyle.ButtonClickSet}>
+            <img className={NavStyle.Settings} src={Gear} alt="Settings" />
+            <span className={NavStyle.SetText}>Settings</span>
+          </ButtonSound>
+        </NavLink>
 
-      <NavLink
-        to="/"
-        className={({ isActive }) =>
-          isActive ? `${NavStyle.navItem} ${NavStyle.activeLink}` : NavStyle.navItem
-        }
-      >
-        <img className={NavStyle.Home} src={Home} alt="Home" />
-        <span className={NavStyle.HomeText}>Home</span>
-      </NavLink>
+        <NavLink to="/"
+          className={({ isActive }) =>`${NavStyle.NavItem} ${isActive ? NavStyle.activeLink : ''}`}>
+          <ButtonSound invisible className={NavStyle.ButtonClickHome}>
+              <img className={NavStyle.Home} src={Home} alt="Home" />
+              <span className={NavStyle.HomeText}>Home</span>
+          </ButtonSound>
+        </NavLink>
 
-      <NavLink
-        to="/instructions"
-        className={({ isActive }) =>
-          isActive ? `${NavStyle.navItem} ${NavStyle.activeLink}` : NavStyle.navItem
-        }
-      >
-        <img className={NavStyle.Info} src={Info} alt="Instructions icon" />
-        <span className={NavStyle.InfoText}>Instructions</span>
-      </NavLink>
-    </nav>
+        <NavLink to="/instructions"
+          className={({ isActive }) =>`${NavStyle.NavItem} ${isActive ? NavStyle.activeLink : ''}`}>
+          <ButtonSound invisible className={NavStyle.ButtonClickInfo}>
+              <img className={NavStyle.Info} src={Info} alt="Info" />
+              <span className={NavStyle.InfoText}>Instructions</span>
+          </ButtonSound>
+        </NavLink>
+      </div>
+    </>
   );
 }
 
--- src/shared/ButtonSound.jsx
+++ src/shared/ButtonSound.jsx
@@ -1,3 +1,32 @@
-export default function ButtonSound () {
+// Global sound for button clicks
+import { useSound } from "../context/SoundProvider";
+import ButtonClick from "../assets/ButtonClick.wav";
 
+export default function ButtonSound ({ onClick, children, className="", invisible, style, ...props}) {
+    const { buttonSoundEnabled } = useSound();
+    const audio = new Audio(ButtonClick);
+    
+    const handleClick = (event) => {
+        if (buttonSoundEnabled) {
+            audio.currentTime = 0;
+            audio.play();
+        }
+        if (onClick) onClick(event);
+    };
+    return (
+        <button onClick={handleClick}{...props}
+        // styling options for inviduall buttons
+        className={`buttonSound${className ? "" + className:""}`}
+        style={
+            invisible ? {
+                background: "none",
+                border: "none",
+                outline: "none",
+                cursor: "pointer",
+                ...style,
+            } : style
+        } >
+            {children}
+        </button>
+    )
 }
