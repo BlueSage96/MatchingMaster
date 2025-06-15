8a9c0815fc89af96d61fd21bbff6333ad108463c
Brittany Price <bprice0496@gmail.com>
2025-06-15 00:20:04 -0400

    Can toggle button and card clicks, updated game play UI

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,17 +1,51 @@
 import { useNavigate } from 'react-router-dom';
 import { useSound } from '../context/SoundProvider';
-import SettingStyles from '../css/modules/Settings.module.css';
+import SettingsStyle from '../css/modules/Settings.module.css';
 import ButtonSound from '../shared/ButtonSound';
+import ButtonClick from '../assets/ButtonClick.wav';
 import CardSound from '../assets/CardFlip.mp3';
 
 function Settings() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
+  const {buttonSoundEnabled, setButtonSoundEnabled, cardSoundEnabled, setCardSoundEnabled} = useSound();
+  const audio = new Audio(ButtonClick);
+  audio.preload = 'auto';
   return (
     <>
       <ButtonSound className="backButton" onClick={() => navigate(-1)}>&larr; Back</ButtonSound>
-      <h1 style={{ fontSize: '60px', position: 'absolute', top: '230px' }}>
-        Settings here
-      </h1>
+      <h1>Settings</h1>
+      <div className={SettingsStyle.Main}>
+        <div className={SettingsStyle.Sound}>
+            <span>Button Sound:</span>
+            <button className={SettingsStyle.Off}
+              onClick={() => {
+                  const newState = !buttonSoundEnabled;
+                  if (newState) {
+                     audio.currentTime = 0;
+                     audio.play();
+                  }
+                  setButtonSoundEnabled(prev => !prev);
+              }}
+              style={{backgroundColor: buttonSoundEnabled ? "green" : "red"}}
+            >{buttonSoundEnabled ? "ON" : "OFF"}</button>
+        </div>
+        <hr className={SettingsStyle.Line}/>
+        <div className={SettingsStyle.Sound}>
+              <span>Card Sound:</span>
+              <button className={SettingsStyle.Off}
+                onClick={() => {
+                    const newState = !cardSoundEnabled;
+                    setCardSoundEnabled(newState);
+                    if (newState) {
+                       const audio = new Audio(CardSound);
+                       audio.play();
+                    }
+                }} style={{backgroundColor: cardSoundEnabled ? "green" : "red"}}
+              >{cardSoundEnabled ? "ON" : "OFF"}</button>
+        </div>
+
+        
+      </div>
     </>
   );
 }
