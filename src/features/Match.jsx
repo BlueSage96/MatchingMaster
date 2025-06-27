92a4fb776b4a5caf53f6383dea3627bf0c18ad83
Brittany Price <bprice0496@gmail.com>
2025-06-26 22:05:09 -0400

    Better shuffle method

--- src/features/Match.jsx
+++ src/features/Match.jsx
@@ -51,7 +51,7 @@ function Match({ playerName, setPlayerName }) {
   function fisherYatesShuffle(array) {
     const shuffled = array.slice();
     for (let i = shuffled.length - 1; i > 0; i--) {
-      const j = Math.floor((Math.random() * (i + 1)));
+      const j = Math.floor((Math.random() * (i)));
       [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
     }
     return shuffled;
